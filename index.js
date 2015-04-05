d23b48d2e6cbaef72695ec9ad5e137cec9ca385d
tunnckoCore <mameto_100@mail.bg>
2015-04-05 21:42:33 +0300

    refactor

--- index.js
+++ index.js
@@ -10,22 +10,10 @@
 var fs = require('fs');
 var path = require('path');
 var ini = require('ini');
-var bluebird = require('bluebird');
-var readFile = bluebird.promisify(fs.readFile);
-var readFileSync = fs.readFileSync;
 
 exports.name = 'ini';
 exports.outputFormat = 'json';
 
 exports.render = function _render(str, opts) {
-  return ini.parse(str, opts);
-};
-exports.renderFile = function _renderFile(filepath, opts) {
-  var input = readFileSync(filepath, 'utf8');
-  return ini.parse(input, opts);
-};
-exports.renderFileAsync = function _renderFileAsync(filepath, opts) {
-  return readFile(filepath, 'utf8').then(function(data) {
-    return ini.parse(data, opts);
-  });
+  return JSON.stringify(ini.parse(str, opts));
 };
